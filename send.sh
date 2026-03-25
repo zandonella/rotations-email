1a36d05586bfdfed38b95a8a49b1f58d6d5026c1
zandonella <123271994+zandonella@users.noreply.github.com>
2026-03-25 14:45:21 -0700

    add script

---
+++ send.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Pulls email logs from the database, sends emails, and updates log statuses.
+
+set -euo pipefail
+
+# cd "C:\Users\zando\Desktop\mythic-shop-api"
+
+node pullSales.ts
+
+npm run send-emails
